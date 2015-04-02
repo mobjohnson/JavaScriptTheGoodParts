291fbb6ee61d24b3e2f0bd46f870e4d007773399
mobjohnson <mobjohnson@gmail.com>
2015-04-02 05:58:27 -0400

    Add object

--- chap01/program.js
+++ chap01/program.js
@@ -1,6 +1,35 @@
+// Chapter 1
+
 document.writeln('Hello, world"');
 
 Function.prototype.method = function (name, func){
   this.prototype[name] = func;
   return this;
 };
+
+// Chapter 3
+
+var empty_object = {};
+
+var stooge = {
+  "fist-name": "Jerome",
+  "last-name": "Howard"
+};
+
+var flight = {
+  airline: "Oceanic",
+  number: 815,
+  departure: {
+    IATA: "SYD",
+    time: "2004-09-22 15:55",
+    city: "Sydney"
+  },
+  arrival: {
+    IATA: "LAX",
+    time: "2004-09-23 10:42",
+    city: "Los Angeles"
+  },
+
+};
+
+
